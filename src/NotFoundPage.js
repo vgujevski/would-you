81096f1256cede83409c81afdc32f48e9439dc9e
Vitali G <gujevski@gmail.com>
2021-10-12 21:36:59 +0300

    style: change message displayed on NotFoundPage
    NotFoundPage will show "404! Something went wrong"
    and a link to go back

--- src/NotFoundPage.js
+++ src/NotFoundPage.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export const NotFoundPage = () => (
-    <div className="content-container">
-      404! - <Link to="/">Back</Link>
+  <div className="content-container">
+    <div className="not-found-page-container">
+      <h2>404! - Something went wrong</h2>
+      <Link className="link" to="/"> go back</Link>
     </div>
+  </div>
 )
